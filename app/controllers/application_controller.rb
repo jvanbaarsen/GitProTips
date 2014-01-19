d61af20f32b54eebe2f5086f126f74d80579be77
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2014-01-19 21:06:23 +0100

    Derp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def staging_authentication
     if ENV['STAGING_AUTH'] == "1"
       authenticate_or_request_with_http_basic do |username, password|
-        username == ENV['STAGING_USER'] && password == ENV['STAGING_PASS']
+        username == ENV['STAGING_USERNAME'] && password == ENV['STAGING_PASSWORD']
       end
     end
   end
