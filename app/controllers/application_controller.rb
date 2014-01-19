99145278c117845de6e47f1e320de8370fb5bb44
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2014-01-19 21:04:18 +0100

    Fixed issue with HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :staging_authentication if ENV['STAGING_AUTH'] == 1
-
+  before_filter :staging_authentication
 
   private
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['STAGING_USER'] && password == ENV['STAGING_PASS']
+  def staging_authentication
+    if ENV['STAGING_AUTH'] == "1"
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['STAGING_USER'] && password == ENV['STAGING_PASS']
+      end
     end
   end
 end
