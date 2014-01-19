0470db82f84b335af950e4fa8edaf167b05f9a57
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2014-01-19 20:58:34 +0100

    Added tips controller and staging lockdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :staging_authentication if ENV['STAGING_AUTH'] == 1
+
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['STAGING_USER'] && password == ENV['STAGING_PASS']
+    end
+  end
 end
