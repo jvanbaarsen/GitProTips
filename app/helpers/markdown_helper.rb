a786e3eca471613390f03ca22baa6289e55328c2
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2014-02-09 22:50:51 +0100

    Added the ability to use Codeblocks in markdown :smile:

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,6 +1,13 @@
 module MarkdownHelper
   def markdown text
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    @markdown ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML,
+      autolink: true,
+      space_after_headers: true,
+      hard_wrap: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true
+    )
     @markdown.render(text).html_safe
   end
 end
--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -6,5 +6,16 @@ describe MarkdownHelper do
       string = 'hi **bold** *italic*'
       expect(markdown(string)).to eq("<p>hi <strong>bold</strong> <em>italic</em></p>\n")
     end
+
+    it 'parsed fenced code blocks' do
+      string = %Q{This is some text
+```
+this is a code block
+```
+      }
+
+      result = %Q{<p>This is some text\n<code>\nthis is a code block\n</code></p>\n}
+      expect(markdown(string)).to eq(result)
+    end
   end
 end
